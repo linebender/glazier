linebender/glazier

commit 3d55067b3b9cb6c09c151969a33f42e9e1d7f9e1

Polish mouse handling
- Use KeyModifiers for mouse events
- use `count` instead of `MouseType`; this is motivated by reusing
`MouseEvent` for mouse_moved, and making `druid_shell::MouseEvent`
closer to `druid::MouseEvent`
- actually thread modifiers through during mouse events

src/mac/mod.rs
@@ -45,7 +45,7 @@ use piet_common::{Piet, RenderContext};
 use crate::keyboard::{KeyEvent, KeyModifiers};
 use crate::platform::dialog::{FileDialogOptions, FileDialogType};
 use crate::util::make_nsstring;
-use crate::window::{MouseButton, MouseEvent, MouseType, WinHandler};
+use crate::window::{MouseButton, MouseEvent, WinHandler};
 use crate::Error;
 
 use util::assert_main_thread;
@@ -276,20 +276,22 @@ extern "C" fn set_frame_size(this: &mut Object, _: Sel, size: NSSize) {
 
 // NOTE: If we know the button (because of the origin call) we pass it through,
 // otherwise we get it from the event itself.
-fn mouse_event(nsevent: id, view: id, ty: MouseType, which: Option<MouseButton>) -> MouseEvent {
+fn mouse_event(nsevent: id, view: id, button: Option<MouseButton>) -> MouseEvent {
     unsafe {
-        let which = which.unwrap_or_else(|| {
-            let which = NSEvent::pressedMouseButtons(nsevent);
-            get_mouse_button(which as usize)
+        let button = button.unwrap_or_else(|| {
+            let button = NSEvent::pressedMouseButtons(nsevent);
+            get_mouse_button(button as usize)
         });
         let point = nsevent.locationInWindow();
         let view_point = view.convertPoint_fromView_(point, nil);
+        let modifiers = nsevent.modifierFlags();
+        let modifiers = make_modifiers(modifiers);
         MouseEvent {
             x: view_point.x as i32,
             y: view_point.y as i32,
-            mods: 0, // TODO
-            which,
-            ty,
+            mods: modifiers,
+            count: nsevent.clickCount() as u32,
+            button,
         }
     }
 }
@@ -322,7 +324,7 @@ fn mouse_down(this: &mut Object, nsevent: id, button: MouseButton) {
     unsafe {
         let view_state: *mut c_void = *this.get_ivar("viewState");
         let view_state = &mut *(view_state as *mut ViewState);
-        let event = mouse_event(nsevent, this as id, MouseType::Down, Some(button));
+        let event = mouse_event(nsevent, this as id, Some(button));
         (*view_state).handler.mouse(&event);
     }
 }
@@ -339,7 +341,7 @@ fn mouse_up(this: &mut Object, nsevent: id, button: MouseButton) {
     unsafe {
         let view_state: *mut c_void = *this.get_ivar("viewState");
         let view_state = &mut *(view_state as *mut ViewState);
-        let event = mouse_event(nsevent, this as id, MouseType::Up, Some(button));
+        let event = mouse_event(nsevent, this as id, Some(button));
         (*view_state).handler.mouse(&event);
     }
 }
@@ -348,10 +350,8 @@ extern "C" fn mouse_move(this: &mut Object, _: Sel, nsevent: id) {
     unsafe {
         let view_state: *mut c_void = *this.get_ivar("viewState");
         let view_state = &mut *(view_state as *mut ViewState);
-        let event = mouse_event(nsevent, this as id, MouseType::Down, None);
-        (*view_state)
-            .handler
-            .mouse_move(event.x as i32, event.y as i32, event.mods);
+        let event = mouse_event(nsevent, this as id, None);
+        (*view_state).handler.mouse_move(&event);
     }
 }
 
@@ -368,7 +368,9 @@ extern "C" fn scroll_wheel(this: &mut Object, _: Sel, nsevent: id) {
                 (dx * 32, dy * 32)
             }
         };
-        let mods = 0; // TODO:
+        let mods = nsevent.modifierFlags();
+        let mods = make_modifiers(mods);
+
         if dx != 0 {
             (*view_state).handler.mouse_hwheel(dx, mods);
         }

src/windows/mod.rs
@@ -61,7 +61,7 @@ use dcomp::{D3D11Device, DCompositionDevice, DCompositionTarget, DCompositionVis
 use dialog::{get_file_dialog_path, FileDialogOptions, FileDialogType};
 
 use crate::keyboard::{KeyCode, KeyEvent, KeyModifiers};
-use crate::window::{self, Cursor, MouseButton, MouseEvent, MouseType, WinHandler};
+use crate::window::{self, Cursor, MouseButton, MouseEvent, WinHandler};
 
 extern "system" {
     pub fn DwmFlush();
@@ -187,7 +187,7 @@ impl Default for PresentStrategy {
     }
 }
 
-/// Must only be called while handling a keyboard input message.
+/// Must only be called while handling an input message.
 /// This queries the keyboard state at the time of message delivery.
 fn get_mod_state() -> KeyModifiers {
     //FIXME: does not handle windows key
@@ -467,21 +467,38 @@ impl WndProc for MyWndProc {
             //TODO: WM_SYSCOMMAND
             WM_MOUSEWHEEL => {
                 let delta = HIWORD(wparam as u32) as i16 as i32;
-                let mods = LOWORD(wparam as u32) as u32;
+                let mods = get_mod_state();
                 self.handler.mouse_wheel(delta, mods);
                 Some(0)
             }
             WM_MOUSEHWHEEL => {
                 let delta = HIWORD(wparam as u32) as i16 as i32;
-                let mods = LOWORD(wparam as u32) as u32;
+                let mods = get_mod_state();
                 self.handler.mouse_hwheel(delta, mods);
                 Some(0)
             }
             WM_MOUSEMOVE => {
                 let x = LOWORD(lparam as u32) as i16 as i32;
                 let y = HIWORD(lparam as u32) as i16 as i32;
-                let mods = LOWORD(wparam as u32) as u32;
-                self.handler.mouse_move(x, y, mods);
+                let mods = get_mod_state();
+                let button = match wparam {
+                    w if (w & 1) > 0 => MouseButton::Left,
+                    w if (w & 1 << 1) > 0 => MouseButton::Right,
+                    w if (w & 1 << 5) > 0 => MouseButton::Middle,
+                    w if (w & 1 << 6) > 0 => MouseButton::X1,
+                    w if (w & 1 << 7) > 0 => MouseButton::X2,
+                    //FIXME: I guess we probably do want `MouseButton::None`?
+                    //this feels bad, but also this gets discarded in druid anyway.
+                    _ => MouseButton::Left,
+                };
+                let event = MouseEvent {
+                    x,
+                    y,
+                    mods,
+                    button,
+                    count: 0,
+                };
+                self.handler.mouse_move(&event);
                 Some(0)
             }
             // TODO: not clear where double-click processing should happen. Currently disabled
@@ -489,7 +506,7 @@ impl WndProc for MyWndProc {
             WM_LBUTTONDBLCLK | WM_LBUTTONDOWN | WM_LBUTTONUP | WM_MBUTTONDBLCLK
             | WM_MBUTTONDOWN | WM_MBUTTONUP | WM_RBUTTONDBLCLK | WM_RBUTTONDOWN | WM_RBUTTONUP
             | WM_XBUTTONDBLCLK | WM_XBUTTONDOWN | WM_XBUTTONUP => {
-                let which = match msg {
+                let button = match msg {
                     WM_LBUTTONDBLCLK | WM_LBUTTONDOWN | WM_LBUTTONUP => MouseButton::Left,
                     WM_MBUTTONDBLCLK | WM_MBUTTONDOWN | WM_MBUTTONUP => MouseButton::Middle,
                     WM_RBUTTONDBLCLK | WM_RBUTTONDOWN | WM_RBUTTONUP => MouseButton::Right,
@@ -504,25 +521,21 @@ impl WndProc for MyWndProc {
                     },
                     _ => unreachable!(),
                 };
-                let ty = match msg {
-                    WM_LBUTTONDOWN | WM_MBUTTONDOWN | WM_RBUTTONDOWN | WM_XBUTTONDOWN => {
-                        MouseType::Down
-                    }
-                    WM_LBUTTONDBLCLK | WM_MBUTTONDBLCLK | WM_RBUTTONDBLCLK | WM_XBUTTONDBLCLK => {
-                        MouseType::DoubleClick
-                    }
-                    WM_LBUTTONUP | WM_MBUTTONUP | WM_RBUTTONUP | WM_XBUTTONUP => MouseType::Up,
+                let count = match msg {
+                    WM_LBUTTONDOWN | WM_MBUTTONDOWN | WM_RBUTTONDOWN | WM_XBUTTONDOWN => 1,
+                    WM_LBUTTONDBLCLK | WM_MBUTTONDBLCLK | WM_RBUTTONDBLCLK | WM_XBUTTONDBLCLK => 2,
+                    WM_LBUTTONUP | WM_MBUTTONUP | WM_RBUTTONUP | WM_XBUTTONUP => 0,
                     _ => unreachable!(),
                 };
                 let x = LOWORD(lparam as u32) as i16 as i32;
                 let y = HIWORD(lparam as u32) as i16 as i32;
-                let mods = LOWORD(wparam as u32) as u32;
+                let mods = get_mod_state();
                 let event = MouseEvent {
                     x,
                     y,
                     mods,
-                    which,
-                    ty,
+                    button,
+                    count,
                 };
                 self.handler.mouse(&event);
                 Some(0)

examples/hello.rs
@@ -19,7 +19,7 @@ use piet_common::kurbo::{Line, Rect};
 use piet_common::{Color, FillRule, RenderContext};
 
 use druid_shell::dialog::{FileDialogOptions, FileDialogType};
-use druid_shell::keyboard::KeyEvent;
+use druid_shell::keyboard::{KeyEvent, KeyModifiers};
 use druid_shell::keycodes::MenuKey;
 use druid_shell::menu::Menu;
 use druid_shell::platform::WindowBuilder;
@@ -71,16 +71,16 @@ impl WinHandler for HelloState {
         false
     }
 
-    fn mouse_wheel(&self, delta: i32, mods: u32) {
-        println!("mouse_wheel {} {:02x}", delta, mods);
+    fn mouse_wheel(&self, delta: i32, mods: KeyModifiers) {
+        println!("mouse_wheel {} {:?}", delta, mods);
     }
 
-    fn mouse_hwheel(&self, delta: i32, mods: u32) {
-        println!("mouse_hwheel {} {:02x}", delta, mods);
+    fn mouse_hwheel(&self, delta: i32, mods: KeyModifiers) {
+        println!("mouse_hwheel {} {:?}", delta, mods);
     }
 
-    fn mouse_move(&self, x: i32, y: i32, mods: u32) {
-        println!("mouse_move ({}, {}) {:02x}", x, y, mods);
+    fn mouse_move(&self, event: &MouseEvent) {
+        println!("mouse_move {:?}", event);
     }
 
     fn mouse(&self, event: &MouseEvent) {

src/window.rs
@@ -17,7 +17,7 @@
 use std::any::Any;
 use std::ops::Deref;
 
-use crate::keyboard::KeyEvent;
+use crate::keyboard::{KeyEvent, KeyModifiers};
 use crate::platform;
 
 // Handle to Window Level Utilities
@@ -81,7 +81,7 @@ pub trait WinHandler {
     ///
     /// The modifiers are the same as WM_MOUSEWHEEL.
     #[allow(unused_variables)]
-    fn mouse_wheel(&self, delta: i32, mods: u32) {}
+    fn mouse_wheel(&self, delta: i32, mods: KeyModifiers) {}
 
     /// Called on a mouse horizontal wheel event. This corresponds to a
     /// [WM_MOUSEHWHEEL](https://msdn.microsoft.com/en-us/library/windows/desktop/ms645614(v=vs.85).aspx)
@@ -89,14 +89,14 @@ pub trait WinHandler {
     ///
     /// The modifiers are the same as WM_MOUSEHWHEEL.
     #[allow(unused_variables)]
-    fn mouse_hwheel(&self, delta: i32, mods: u32) {}
+    fn mouse_hwheel(&self, delta: i32, mods: KeyModifiers) {}
 
     /// Called when the mouse moves. Note that the x, y coordinates are
     /// in absolute pixels.
     ///
     /// TODO: should we reuse the MouseEvent struct for this method as well?
     #[allow(unused_variables)]
-    fn mouse_move(&self, x: i32, y: i32, mods: u32) {}
+    fn mouse_move(&self, event: &MouseEvent) {}
 
     /// Called on mouse button up or down. Note that the x, y
     /// coordinates are in absolute pixels.
@@ -112,7 +112,7 @@ pub trait WinHandler {
     fn as_any(&self) -> &dyn Any;
 }
 
-/// A mouse button press or release event.
+/// The state of the mouse for a click, mouse-up, or move event.
 #[derive(Debug)]
 pub struct MouseEvent {
     /// X coordinate in absolute pixels.
@@ -120,11 +120,13 @@ pub struct MouseEvent {
     /// Y coordinate in absolute pixels.
     pub y: i32,
     /// Modifiers, as in raw WM message
-    pub mods: u32,
-    /// Which button was pressed or released.
-    pub which: MouseButton,
-    /// Type of event.
-    pub ty: MouseType,
+    pub mods: KeyModifiers,
+    /// The number of mouse clicks associated with this event. This will always
+    /// be `0` for a mouse-up event.
+    pub count: u32,
+    /// The currently pressed button in the case of a move or click event,
+    /// or the released button in the case of a mouse-up event.
+    pub button: MouseButton,
 }
 
 /// An indicator of which mouse button was pressed.
@@ -142,18 +144,6 @@ pub enum MouseButton {
     X2,
 }
 
-/// An indicator of the state change of a mouse button.
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
-pub enum MouseType {
-    /// Mouse down event.
-    Down,
-    /// Note: DoubleClick is currently disabled, as we don't use the
-    /// Windows processing.
-    DoubleClick,
-    /// Mouse up event.
-    Up,
-}
-
 /// Standard cursor types. This is only a subset, others can be added as needed.
 pub enum Cursor {
     Arrow,
@@ -168,5 +158,5 @@ pub struct ScrollEvent {
     /// The scroll wheel’s vertical delta.
     pub dy: f64,
     /// Modifiers, as in raw WM message
-    pub mods: u32,
+    pub mods: KeyModifiers,
 }

commit 9dccf5737ad299b8c52a7f91aef0d4159899e434

Unbreak macOS mouse-up
This fixes a regression in 3d55067, where we were incorrectly using
the `NSEvent.clickCount` method even in the case of mouseUp events,
which (per druid) should always have a count of 0.

src/mac/mod.rs
@@ -276,7 +276,7 @@ extern "C" fn set_frame_size(this: &mut Object, _: Sel, size: NSSize) {
 
 // NOTE: If we know the button (because of the origin call) we pass it through,
 // otherwise we get it from the event itself.
-fn mouse_event(nsevent: id, view: id, button: Option<MouseButton>) -> MouseEvent {
+fn mouse_event(nsevent: id, view: id, down: bool, button: Option<MouseButton>) -> MouseEvent {
     unsafe {
         let button = button.unwrap_or_else(|| {
             let button = NSEvent::pressedMouseButtons(nsevent);
@@ -286,11 +286,12 @@ fn mouse_event(nsevent: id, view: id, button: Option<MouseButton>) -> MouseEvent
         let view_point = view.convertPoint_fromView_(point, nil);
         let modifiers = nsevent.modifierFlags();
         let modifiers = make_modifiers(modifiers);
+        let count = if down { nsevent.clickCount() as u32 } else { 0 };
         MouseEvent {
             x: view_point.x as i32,
             y: view_point.y as i32,
             mods: modifiers,
-            count: nsevent.clickCount() as u32,
+            count,
             button,
         }
     }
@@ -324,7 +325,7 @@ fn mouse_down(this: &mut Object, nsevent: id, button: MouseButton) {
     unsafe {
         let view_state: *mut c_void = *this.get_ivar("viewState");
         let view_state = &mut *(view_state as *mut ViewState);
-        let event = mouse_event(nsevent, this as id, Some(button));
+        let event = mouse_event(nsevent, this as id, true, Some(button));
         (*view_state).handler.mouse(&event);
     }
 }
@@ -341,7 +342,7 @@ fn mouse_up(this: &mut Object, nsevent: id, button: MouseButton) {
     unsafe {
         let view_state: *mut c_void = *this.get_ivar("viewState");
         let view_state = &mut *(view_state as *mut ViewState);
-        let event = mouse_event(nsevent, this as id, Some(button));
+        let event = mouse_event(nsevent, this as id, false, Some(button));
         (*view_state).handler.mouse(&event);
     }
 }
@@ -350,7 +351,7 @@ extern "C" fn mouse_move(this: &mut Object, _: Sel, nsevent: id) {
     unsafe {
         let view_state: *mut c_void = *this.get_ivar("viewState");
         let view_state = &mut *(view_state as *mut ViewState);
-        let event = mouse_event(nsevent, this as id, None);
+        let event = mouse_event(nsevent, this as id, false, None);
         (*view_state).handler.mouse_move(&event);
     }
 }