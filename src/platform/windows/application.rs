linebender/glazier

commit cf8d3d33e254ec7bf2f72ca919c42ff307d5ef9d

Unify druid-shell RunLoop and Application types

src/platform/windows/application.rs
@@ -14,13 +14,20 @@
 
 //! Windows implementation of features at the application scope.
 
+use std::mem;
+use std::ptr;
+
 use winapi::shared::minwindef::HINSTANCE;
 use winapi::shared::ntdef::LPCWSTR;
 use winapi::shared::windef::HCURSOR;
 use winapi::um::shellscalingapi::PROCESS_SYSTEM_DPI_AWARE;
 use winapi::um::wingdi::CreateSolidBrush;
-use winapi::um::winuser::{LoadIconW, PostQuitMessage, RegisterClassW, IDI_APPLICATION, WNDCLASSW};
+use winapi::um::winuser::{
+    DispatchMessageW, GetAncestor, GetMessageW, LoadIconW, PostQuitMessage, RegisterClassW,
+    TranslateAcceleratorW, TranslateMessage, GA_ROOT, IDI_APPLICATION, MSG, WNDCLASSW,
+};
 
+use super::accels;
 use super::clipboard::Clipboard;
 use super::util::{self, ToWide, CLASS_NAME, OPTIONAL_FUNCTIONS};
 use super::window::win_proc_dispatch;
@@ -28,8 +35,37 @@ use super::window::win_proc_dispatch;
 pub struct Application;
 
 impl Application {
+    pub fn new() -> Application {
+        Application
+    }
+
+    pub fn run(&mut self) {
+        Application::init();
+        unsafe {
+            // Handle windows messages
+            loop {
+                let mut msg = mem::MaybeUninit::uninit();
+                // Note: we could use PM_REMOVE here and avoid the GetMessage below
+                let res = GetMessageW(msg.as_mut_ptr(), ptr::null_mut(), 0, 0);
+                if res <= 0 {
+                    return;
+                }
+                let mut msg: MSG = msg.assume_init();
+                let accels = accels::find_accels(GetAncestor(msg.hwnd, GA_ROOT));
+                let translated = accels.map_or(false, |it| {
+                    TranslateAcceleratorW(msg.hwnd, it.handle(), &mut msg) != 0
+                });
+
+                if !translated {
+                    TranslateMessage(&msg);
+                    DispatchMessageW(&msg);
+                }
+            }
+        }
+    }
+
     /// Initialize the app. At the moment, this is mostly needed for hi-dpi.
-    pub fn init() {
+    fn init() {
         util::attach_console();
         if let Some(func) = OPTIONAL_FUNCTIONS.SetProcessDpiAwareness {
             // This function is only supported on windows 10

src/application.rs
@@ -20,12 +20,18 @@ use crate::platform::application as platform;
 //TODO: we may want to make the user create an instance of this (Application::global()?)
 //but for now I'd like to keep changes minimal.
 /// The top level application object.
-pub struct Application;
+pub struct Application(platform::Application);
 
 impl Application {
-    /// Initialize the app. At the moment, this is mostly needed for hi-dpi.
-    pub fn init() {
-        platform::Application::init()
+    pub fn new() -> Application {
+        Application(platform::Application::new())
+    }
+
+    /// Start the runloop.
+    ///
+    /// This will block the current thread until the program has finished executing.
+    pub fn run(&mut self) {
+        self.0.run()
     }
 
     /// Terminate the application.

src/platform/gtk/application.rs
@@ -14,22 +14,68 @@
 
 //! GTK implementation of features at the application scope.
 
-use gtk::GtkApplicationExt;
+use std::cell::RefCell;
+
+use gio::prelude::ApplicationExtManual;
+use gio::{ApplicationExt, ApplicationFlags, Cancellable};
+use gtk::{Application as GtkApplication, GtkApplicationExt};
 
 use super::clipboard::Clipboard;
-use super::runloop;
 use super::util;
+
+// XXX: The application needs to be global because WindowBuilder::build wants
+// to construct an ApplicationWindow, which needs the application, but
+// WindowBuilder::build does not get the RunLoop
+thread_local!(
+    static GTK_APPLICATION: RefCell<Option<GtkApplication>> = RefCell::new(None);
+);
 
 pub struct Application;
 
 impl Application {
-    pub fn init() {
+    pub fn new() -> Application {
         gtk::init().expect("GTK initialization failed");
+        util::assert_main_thread();
+
+        // TODO: we should give control over the application ID to the user
+        let application = GtkApplication::new(
+            Some("com.github.xi-editor.druid"),
+            // TODO we set this to avoid connecting to an existing running instance
+            // of "com.github.xi-editor.druid" after which we would never receive
+            // the "Activate application" below. See pull request druid#384
+            // Which shows another way once we have in place a mechanism for
+            // communication with remote instances.
+            ApplicationFlags::NON_UNIQUE,
+        )
+        .expect("Unable to create GTK application");
+
+        application.connect_activate(|_app| {
+            log::info!("gtk: Activated application");
+        });
+
+        application
+            .register(None as Option<&Cancellable>)
+            .expect("Could not register GTK application");
+
+        GTK_APPLICATION.with(move |x| *x.borrow_mut() = Some(application));
+        Application
+    }
+
+    pub fn run(&mut self) {
+        util::assert_main_thread();
+
+        // TODO: should we pass the command line arguments?
+        GTK_APPLICATION.with(|x| {
+            x.borrow()
+                .as_ref()
+                .unwrap() // Safe because we initialized this in RunLoop::new
+                .run(&[])
+        });
     }
 
     pub fn quit() {
         util::assert_main_thread();
-        runloop::with_application(|app| {
+        with_application(|app| {
             match app.get_active_window() {
                 None => {
                     // no application is running, main is not running
@@ -50,4 +96,19 @@ impl Application {
         //TODO ahem
         "en-US".into()
     }
+}
+
+#[inline]
+pub(crate) fn with_application<F, R>(f: F) -> R
+where
+    F: std::ops::FnOnce(GtkApplication) -> R,
+{
+    util::assert_main_thread();
+    GTK_APPLICATION.with(move |app| {
+        let app = app
+            .borrow()
+            .clone()
+            .expect("Tried to manipulate the application before RunLoop::new was called");
+        f(app)
+    })
 }

src/platform/mac/application.rs
@@ -19,19 +19,34 @@
 use super::clipboard::Clipboard;
 use super::util;
 
-use cocoa::appkit::NSApp;
+use cocoa::appkit::{NSApp, NSApplication, NSApplicationActivationPolicyRegular};
 use cocoa::base::{id, nil, YES};
+use cocoa::foundation::NSAutoreleasePool;
 use objc::declare::ClassDecl;
 use objc::runtime::{Class, Object, Sel};
 
-pub struct Application;
+pub struct Application {
+    ns_app: id,
+}
 
 impl Application {
-    pub fn init() {
+    pub fn new() -> Application {
+        util::assert_main_thread();
         unsafe {
+            let _pool = NSAutoreleasePool::new(nil);
+
             let delegate: id = msg_send![APP_DELEGATE.0, alloc];
             let () = msg_send![delegate, init];
-            let () = msg_send![NSApp(), setDelegate: delegate];
+            let ns_app = NSApp();
+            let () = msg_send![ns_app, setDelegate: delegate];
+            ns_app.setActivationPolicy_(NSApplicationActivationPolicyRegular);
+            Application { ns_app }
+        }
+    }
+
+    pub fn run(&mut self) {
+        unsafe {
+            self.ns_app.run();
         }
     }
 

examples/perftest.rs
@@ -19,7 +19,7 @@ use time::Time;
 use piet_common::kurbo::{Line, Rect};
 use piet_common::{Color, FontBuilder, Piet, RenderContext, Text, TextLayoutBuilder};
 
-use druid_shell::{Application, KeyEvent, RunLoop, WinHandler, WindowBuilder, WindowHandle};
+use druid_shell::{Application, KeyEvent, WinHandler, WindowBuilder, WindowHandle};
 
 const BG_COLOR: Color = Color::rgb8(0x27, 0x28, 0x22);
 const FG_COLOR: Color = Color::rgb8(0xf0, 0xf0, 0xea);
@@ -108,9 +108,7 @@ impl WinHandler for PerfTest {
 }
 
 fn main() {
-    Application::init();
-
-    let mut run_loop = RunLoop::new();
+    let mut app = Application::new();
     let mut builder = WindowBuilder::new();
     let perf_test = PerfTest {
         size: Default::default(),
@@ -122,5 +120,5 @@ fn main() {
 
     let window = builder.build().unwrap();
     window.show();
-    run_loop.run();
+    app.run();
 }

examples/shello.rs
@@ -19,7 +19,7 @@ use druid_shell::piet::{Color, RenderContext};
 
 use druid_shell::{
     Application, Cursor, FileDialogOptions, FileSpec, HotKey, KeyEvent, KeyModifiers, Menu,
-    MouseEvent, RunLoop, SysMods, TimerToken, WinHandler, WindowBuilder, WindowHandle,
+    MouseEvent, SysMods, TimerToken, WinHandler, WindowBuilder, WindowHandle,
 };
 
 const BG_COLOR: Color = Color::rgb8(0x27, 0x28, 0x22);
@@ -109,8 +109,6 @@ impl WinHandler for HelloState {
 }
 
 fn main() {
-    Application::init();
-
     let mut file_menu = Menu::new();
     file_menu.add_item(
         0x100,
@@ -130,7 +128,7 @@ fn main() {
     menubar.add_dropdown(Menu::new(), "Application", true);
     menubar.add_dropdown(file_menu, "&File", true);
 
-    let mut run_loop = RunLoop::new();
+    let mut app = Application::new();
     let mut builder = WindowBuilder::new();
     builder.set_handler(Box::new(HelloState::default()));
     builder.set_title("Hello example");
@@ -138,5 +136,5 @@ fn main() {
     let window = builder.build().unwrap();
 
     window.show();
-    run_loop.run();
+    app.run();
 }

src/lib.rs
@@ -47,7 +47,6 @@ mod keycodes;
 mod menu;
 mod mouse;
 mod platform;
-mod runloop;
 mod window;
 
 pub use application::Application;
@@ -60,7 +59,6 @@ pub use keyboard::{KeyEvent, KeyModifiers};
 pub use keycodes::KeyCode;
 pub use menu::Menu;
 pub use mouse::{Cursor, MouseButton, MouseEvent};
-pub use runloop::RunLoop;
 pub use window::{
     IdleHandle, IdleToken, Text, TimerToken, WinHandler, WindowBuilder, WindowHandle,
 };

src/platform/gtk/mod.rs
@@ -20,6 +20,5 @@ pub mod dialog;
 pub mod error;
 pub mod keycodes;
 pub mod menu;
-pub mod runloop;
 pub mod util;
 pub mod window;

commit 17a18203f476716741a8157afde63bffe4206c09

Correctly init application on win7
This corrects an error introduced in cf8d3d3, where Application::init
was not being called in the correct place.

fixes #555

src/platform/windows/application.rs
@@ -36,16 +36,15 @@ pub struct Application;
 
 impl Application {
     pub fn new() -> Application {
+        Application::init();
         Application
     }
 
     pub fn run(&mut self) {
-        Application::init();
         unsafe {
             // Handle windows messages
             loop {
                 let mut msg = mem::MaybeUninit::uninit();
-                // Note: we could use PM_REMOVE here and avoid the GetMessage below
                 let res = GetMessageW(msg.as_mut_ptr(), ptr::null_mut(), 0, 0);
                 if res <= 0 {
                     return;