3f35ff312719428b3d494a5ea092339e8b0da2e3
Joe Neeman <joeneeman@gmail.com>
2023-01-07 10:33:10 -0600

    fix clippy on wayland

--- src/backend/wayland/window.rs
+++ src/backend/wayland/window.rs
@@ -305,10 +305,10 @@ impl Default for WindowHandle {
         WindowHandle {
             inner: std::sync::Arc::new(Inner {
                 id: surfaces::GLOBAL_ID.next(),
-                outputs: Box::new(surfaces::surface::Dead::default()),
-                decor: Box::new(surfaces::surface::Dead::default()),
-                surface: Box::new(surfaces::surface::Dead::default()),
-                popup: Box::new(surfaces::surface::Dead::default()),
+                outputs: Box::<surfaces::surface::Dead>::default(),
+                decor: Box::<surfaces::surface::Dead>::default(),
+                surface: Box::<surfaces::surface::Dead>::default(),
+                popup: Box::<surfaces::surface::Dead>::default(),
                 appdata: std::sync::Weak::new(),
             }),
         }
