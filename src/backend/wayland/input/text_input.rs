f964690046fc7343d65c546139c3ecf4a37db0fc
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2023-07-15 22:36:23 +0100

    Sort out `sync_state`

--- src/backend/wayland/input/text_input.rs
+++ src/backend/wayland/input/text_input.rs
@@ -1,3 +1,4 @@
+use memchr::memmem::find;
 use smithay_client_toolkit::reexports::{
     client::{protocol::wl_seat, Dispatch, QueueHandle},
     protocols::wp::text_input::{
@@ -12,6 +13,7 @@ use smithay_client_toolkit::reexports::{
 use crate::{
     backend::wayland::{window::WindowId, WaylandState},
     text::{Affinity, InputHandler},
+    TextFieldToken,
 };
 
 use super::SeatName;
@@ -21,33 +23,127 @@ struct InputUserData(SeatName);
 pub(super) struct InputState {
     text_input: ZwpTextInputV3,
     active_window: Option<WindowId>,
+
+    // Wayland requires that we store all the state sent in requests,
+    // and then apply them in the `done` message
+    // These are the versions of these values which 'we' own - not the
+    // versions passed to the input field
+    // These will be applied in done, unless reset
+    commit_string: Option<String>,
+    preedit_string: Option<String>,
+    delete_surrounding_before: u32,
+    delete_surrounding_after: u32,
+    new_cursor_begin: i32,
+    new_cursor_end: i32,
+
+    // The bookkeeping state
+    /// Used for sanity checking - the token we believe we're operating on,
+    /// which the bookkeeping state is relative to
+    token: Option<TextFieldToken>,
+    /// The position in the input lock buffer (of token) where the compositor
+    /// believes the buffer to start from. See [set_surrounding_text], which shows
+    /// that Wayland wants only a subset of the full text ("additional characters")
+    /// but not the full buffer
+    ///
+    /// [set_surrounding_text]: https://wayland.app/protocols/text-input-unstable-v3#zwp_text_input_v3:request:set_surrounding_text
+    buffer_start: usize,
 }
 
 impl InputState {
-    fn sync_state(&mut self, handler: Box<dyn InputHandler>) {
+    fn reset(&mut self) {
+        self.commit_string = None;
+        self.preedit_string = None;
+        self.delete_surrounding_before = 0;
+        self.delete_surrounding_after = 0;
+        self.new_cursor_begin = 0;
+        self.new_cursor_end = 0;
+    }
+
+    fn sync_state(
+        &mut self,
+        handler: Box<dyn InputHandler>,
+        cause: zwp_text_input_v3::ChangeCause,
+    ) {
         // input_state.text_input.set_content_type();
         let selection = handler.selection();
 
-        let selection_range = selection.range();
-        // TODO: Confirm these affinities
-        let start_line = handler.line_range(selection_range.start, Affinity::Downstream);
-        let end_line = handler.line_range(selection_range.end, Affinity::Upstream);
-        let complete_range = start_line.start..end_line.end;
+        // TODO: Should we just
+        let preedit_range = handler.composition_range();
 
-        let text = handler.slice(complete_range.clone());
-        // Wayland strings cannot be longer than 4000 bytes
-        // Give some margin for error
-        if text.len() > 3800 {
-            // Best effort attempt here?
-        } else {
+        let selection_range = selection.range();
+        // TODO: Confirm these affinities. I suspect all combinations of choices are wrong here, but oh well
+        let start_line = handler.line_range(selection_range.start, Affinity::Upstream);
+        let end_line = handler.line_range(selection_range.end, Affinity::Downstream);
+        let mut complete_range = start_line.start..end_line.end;
+
+        'can_set_surrounding_text: {
+            // Wayland strings cannot be longer than 4000 bytes
+            // Give some margin for error
+            if complete_range.len() > 3800 {
+                // Best effort attempt here?
+                if selection_range.len() > 3800 {
+                    // If the selection range is too big, the protocol seems not to support this
+                    // Just don't send it then
+                    // Luckily, the set_surrounding_text isn't needed, and
+                    // pre-edit text will soon be deleted
+                    break 'can_set_surrounding_text;
+                }
+                let find_boundary = |mut it| {
+                    let mut iterations = 0;
+                    loop {
+                        if handler.is_char_boundary(it) {
+                            break it;
+                        }
+                        if iterations > 10 {
+                            panic!("is_char_boundary implemented incorrectly");
+                        }
+                        it += 1;
+                        iterations += 1;
+                    }
+                };
+                // 🤷 this is probably "additional characters"
+                complete_range = find_boundary((selection_range.start - 50).max(start_line.start))
+                    ..find_boundary((selection_range.end + 50).min(end_line.end));
+            }
+            let start_range;
+            let end_range;
+            let mut final_selection = selection;
+            if let Some(excluded_range) = handler.composition_range() {
+                // The API isn't clear on what should happen if the selection is changed (e.g. by the mouse)
+                // whilst an edit is ongoing. Because of this, we choose to commit the pre-edit text when this happens
+                // (i.e. Event::SelectionChanged). This does mean that the potentially inconsistent pre-edit
+                // text is inserted into the text, but in my mind this is better than alternatives.
+                // Because of this behaviour, if pre-edit text has been sent to the client, we know that the selection is empty
+                // (because it either was replaced by the pre-edit text, or was)
+
+                // However, upon testing to validate this approach, it was discovered that GNOME doesn't implement their
+                // Wayland text input API properly, as it does nothing with the value from the set_text_change_cause request
+                // Because of this, as well as the commit, the IME follows the new input.
+                assert_eq!(
+                    final_selection.active, final_selection.anchor,
+                    "Glazier: Inconsistent state. If the selection changes, you must call update_text_field"
+                );
+                assert_eq!(final_selection.active, excluded_range.end);
+                final_selection.active = excluded_range.start;
+                final_selection.anchor = excluded_range.start;
+                start_range = complete_range.start..excluded_range.start;
+                end_range = excluded_range.end..complete_range.end;
+            } else {
+                start_range = complete_range;
+                end_range = 0..0;
+            }
+            let mut text = handler.slice(start_range.clone()).into_owned();
+            if !end_range.is_empty() {
+                text.push_str(&handler.slice(end_range.clone()));
+            }
             self.text_input.set_surrounding_text(
-                text.into_owned(),
-                (selection.active - complete_range.start) as i32,
-                (selection.anchor - complete_range.end) as i32,
+                text,
+                (final_selection.active - complete_range.start) as i32,
+                (final_selection.anchor - complete_range.start) as i32,
             );
         }
 
-        let range = handler.slice_bounding_box(selection.range());
+        let range = handler.slice_bounding_box(selection_range);
         if let Some(range) = range {
             let x = range.min_x();
             let y = range.min_y();
@@ -58,6 +154,8 @@ impl InputState {
                 (range.max_y() - y) as i32,
             );
         };
+
+        self.text_input.set_text_change_cause(cause);
 
         self.text_input.commit();
     }
@@ -73,6 +171,10 @@ impl InputState {
         InputState {
             text_input: manager.get_text_input(seat, qh, InputUserData(seat_name)),
             active_window: None,
+            delete_surrounding_after: 0,
+            delete_surrounding_before: 0,
+            commit_string: None,
+            preedit_string: None,
         }
     }
 }
@@ -112,7 +214,8 @@ impl Dispatch<ZwpTextInputV3, InputUserData> for WaylandState {
                 let input_lock = win.get_input_lock(false);
                 if let Some((handler, token)) = input_lock {
                     input_state.text_input.enable();
-                    input_state.sync_state(handler);
+                    // ChangeCause is Other here, because the input editor has not sent the text
+                    input_state.sync_state(handler, zwp_text_input_v3::ChangeCause::Other);
                     win.release_input_lock(token);
                 }
             }
@@ -121,6 +224,7 @@ impl Dispatch<ZwpTextInputV3, InputUserData> for WaylandState {
                 let Some(win) = state.windows.get_mut(&window_id) else {return;};
                 win.remove_input_seat(data.0);
                 let text_input = state.text_input(data);
+                text_input.reset();
                 text_input.active_window = None;
                 // These don't seem to be necessary here
                 // text_input.text_input.disable();
@@ -131,25 +235,8 @@ impl Dispatch<ZwpTextInputV3, InputUserData> for WaylandState {
                 cursor_begin,
                 cursor_end,
             } => {
-                // We need to inline these to make the borrow checker happy :(
-                let input_state = state
-                    .input_states
-                    .iter_mut()
-                    .find(|it| it.id == data.0)
-                    .expect("Glazier: Internal error, accessed deleted seat")
-                    .input_state
-                    .as_mut()
-                    .unwrap();
-                let window_id = input_state.active_window.as_ref().unwrap();
-                let win = state.windows.get_mut(window_id).unwrap();
-                if let Some((handler, token)) = win.get_input_lock(true) {
-                    handler.set_composition_range(range);
-
-                    win.release_input_lock(token);
-                } else {
-                    // Could there be a race condition between disabling text input and informing the server?
-                    tracing::error!("Got text_input event whilst input should be disabled");
-                }
+                let input_state = state.text_input(data);
+                input_state.preedit_string = text;
             }
             zwp_text_input_v3::Event::CommitString { text } => todo!(),
             zwp_text_input_v3::Event::DeleteSurroundingText {
